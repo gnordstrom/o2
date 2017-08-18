9db20eb2dbc4644c7b22f1bd93475d4a3bf8ef32
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-08-18 12:19:40 -0600

    Completed header, responsive

--- src/components/Home/NavBar/NavBar.js
+++ src/components/Home/NavBar/NavBar.js
@@ -6,6 +6,10 @@ import cart from './assets/cart.svg'
 import glass from './assets/glass.svg'
 import ham from './assets/ham.svg'
 import logo from './assets/logo.svg'
+import logoWords from './assets/logo-words.svg'
+import account from './assets/account.svg'
+import review from './assets/review.svg'
+import list from './assets/list.svg'
 
 class NavBar extends Component {
   render() {
@@ -17,15 +21,40 @@ class NavBar extends Component {
           </div>
           <div className="box b">
             <img src={logo}/>
-
+          </div>
+          <div className="box e">
+            <img src={logoWords} />
           </div>
           <div className="box c">
             <input type="text" placeholder="Search"/>
-            <img src={glass}/>
+            <span className="red-box">
+              <img src={glass}/>
+            </span>
+            <span className="no-box">
+              <img src={glass}/>
+            </span>
+          </div>
+
+          <div className="box i">
+            <span className="i-top">Get Paid to Shop</span>
+            <span className="i-bottom">Start Earning</span>
+          </div>
+
+          <div className="box f">
+            <img src={account}/>
+            <label>Account</label>
+          </div>
+          <div className="box g">
+            <img src={review}/>
+            <label>Reviews</label>
+          </div>
+          <div className="box h">
+            <img src={list}/>
+            <label>Lists</label>
           </div>
           <div className="box d">
             <img src={cart} />
-
+            <label className="hidden">Cart</label>
           </div>
         </div>
       </div>
