7713262d8cec0bd4b25b28246eecdd738a3774eb
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-08-22 10:59:29 -0600

    completed mobile part of featuredSales component, styled

--- src/components/Home/FeaturedSales/FeaturedSales.js
+++ src/components/Home/FeaturedSales/FeaturedSales.js
@@ -2,11 +2,99 @@ import React, { Component } from 'react'
 
 import './FeaturedSales.css'
 
+import a from './assets/1.jpg'
+import b from './assets/2.jpg'
+import c from './assets/3.jpg'
+import d from './assets/4.jpg'
+import e from './assets/5.jpg'
+import f from './assets/6.jpg'
+
 class FeaturedSales extends Component {
   render() {
     return (
-      <div>
-        <h1>Featured</h1>
+      <div className="fs-wrapper bg-gray">
+        <div className="fs-header">
+          Shop our Featured Sales
+        </div>
+
+        <div className="fs-card bg-white">
+          <a href="#" className="fs-card-link">
+            <div className="fs-card-left">
+              <strong>Up to Extra 15% Off</strong>
+              <br /> 
+              Furniture*&nbsp;›
+            </div>
+            <div className="fs-card-right">
+              <img src={a} />
+            </div>
+          </a>
+        </div>
+        
+        <div className="fs-card bg-white">
+        <a href="#" className="fs-card-link">
+            <div className="fs-card-left">
+              <strong>Up to Extra 20% Off</strong>
+              <br /> 
+              Area Rugs*&nbsp;›
+            </div>
+            <div className="fs-card-right">
+              <img src={b} />
+            </div>
+          </a>
+        </div>
+        
+        <div className="fs-card bg-white">
+        <a href="#" className="fs-card-link">
+            <div className="fs-card-left">
+              <strong>Up to Extra 20% Off</strong>
+              <br /> 
+              Home Decor*&nbsp;›
+            </div>
+            <div className="fs-card-right">
+              <img src={c} />
+            </div>
+          </a>
+        </div>
+        <div className="fs-card bg-white">
+          <a href="#" className="fs-card-link">
+            <div className="fs-card-left">
+              <strong>Up to Extra 15% Off</strong>
+              <br /> 
+              Bedding & Bath*&nbsp;›
+            </div>
+            <div className="fs-card-right">
+              <img src={d} />
+            </div>
+          </a>
+        </div>
+        
+        <div className="fs-card bg-white">
+        <a href="#" className="fs-card-link">
+            <div className="fs-card-left">
+              <strong>Up to Extra 15% Off</strong>
+              <br /> 
+              Mattresses & Memory Foam*&nbsp;›
+            </div>
+            <div className="fs-card-right">
+              <img src={e} />
+            </div>
+          </a>
+        </div>
+        
+        <div className="fs-card bg-white">
+        <a href="#" className="fs-card-link">
+            <div className="fs-card-left">
+              <strong>Up to Extra 15% Off</strong>
+              <br /> 
+              Garden & Patio*&nbsp;›
+            </div>
+            <div className="fs-card-right">
+              <img src={f} />
+            </div>
+          </a>
+        </div>
+
+
       </div>
     );
   }
--- src/components/Home/TopCarousel/TopCarousel.js
+++ src/components/Home/TopCarousel/TopCarousel.js
@@ -5,7 +5,7 @@ import './TopCarousel.css'
 class TopCarousel extends Component {
   render() {
     return (
-      <div className="carousel-wrapper">
+      <div className="carousel-wrapper bg-gray">
         <div className="carousel-img"></div>
 
       </div>
